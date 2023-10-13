298f9eecf19a6cb488dbb9c8bd1810ed1416aac3
Sang Tran <sangtran@ancient8.gg>
2023-10-13 15:54:22 +0700

    feat: create sdk & typedoc

--- src/tree.ts
+++ src/tree.ts
@@ -22,7 +22,25 @@ export class Tree {
    * The merkle root.
    */
   get root(): Node {
-    // TODO
+    let nodes = this.leaves.map((leaf) => new Node(leaf.value))
+
+    if (nodes.length === 0)
+      throw new Error('Cannot generate merkle root from empty tree.')
+
+    if (nodes.length === 1) return nodes[0]
+
+    while (nodes.length > 1) {
+      const temp: Node[] = []
+      for (let i = 0; i < nodes.length; i += 2) {
+        if (!nodes[i + 1]) {
+          temp.push(nodes[i])
+          break
+        }
+        temp.push(nodes[i].hash(nodes[i + 1]))
+      }
+      nodes = temp
+    }
+    return nodes[0]
   }
 
   /**
@@ -31,7 +49,46 @@ export class Tree {
    * @returns Proof - The list of nodes.
    */
   prove(leaf: Leaf): Node[] {
-    // TODO
+    let proof: Node[] = []
+    let nodes = this.leaves.map((leaf) => new Node(leaf.value))
+    let nodeProved = new Node(leaf.value)
+    const merkleTreeLevel = Math.ceil(Math.log2(nodes.length))
+
+    /* Pre-validate */
+    if (nodes.length === 0)
+      throw new Error('Cannot find prove from empty tree!')
+    if (nodes.length === 1) return proof
+
+    /* Generate proof */
+    let sibling: Node | undefined = undefined
+
+    for (let i = 0; i < merkleTreeLevel; i++) {
+      //Step 1: find index of nodeProved
+      let provedIdx = nodes.findIndex((node) => node.eq(nodeProved))
+
+      //Step 2: find sibling
+      if (provedIdx % 2 === 0 && provedIdx + 1 < nodes.length) {
+        sibling = nodes[provedIdx + 1]
+      } else {
+        sibling = nodes[provedIdx - 1]
+      }
+
+      //Step 3: update nodeProved and proof
+      if (sibling) {
+        nodeProved = nodeProved.hash(sibling)
+        proof.push(sibling)
+      }
+
+      //Step 4: build next level of nodes
+      const nextLevel: Node[] = []
+      for (let i = 0; i < nodes.length; i += 2) {
+        if (i + 1 < nodes.length) nextLevel.push(nodes[i].hash(nodes[i + 1]))
+        else nextLevel.push(nodes[i])
+      }
+      nodes = nextLevel
+    }
+
+    return proof
   }
 
   /**
@@ -41,6 +98,8 @@ export class Tree {
    * @returns true/false
    */
   verify(leaf: Leaf, proof: Node[]): boolean {
-    // TODO
+    let node = new Node(leaf.value)
+    for (let i = 0; i < proof.length; i++) node = node.hash(proof[i])
+    return this.root.eq(node)
   }
 }
